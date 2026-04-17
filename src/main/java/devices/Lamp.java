eb7d423a01227e62e4733a19de18df40b4a51509
Fover <limberfinn@gmail.com>
2026-04-17 09:25:23 +0200

    creating lamp device

---
+++ src/main/java/devices/Lamp.java
@@ -0,0 +1,68 @@
+package devices;
+
+import model.AbstractDevice;
+import model.DeviceFunction;
+import model.Room;
+
+public class Lamp extends AbstractDevice {
+    private int helligkeit = 0;
+    private boolean isAn = false;
+
+    public Lamp(String id, String name, Room room) {
+        super(id, name, room);
+
+//         Strategie 1: Ein-/Ausschalten
+        this.functions.put("Schalten", new DeviceFunction() {
+            @Override
+            public void execute(Object parameter) {
+                if (parameter instanceof Boolean) {
+                    isAn = (Boolean) parameter;
+                    // Wenn ausgeschaltet, Helligkeit auf 0, sonst z.B. auf 100
+                    helligkeit = isAn ? 100 : 0;
+                }
+            }
+
+            @Override
+            public String getDescription() {
+                return "Schaltet die Lampe ein oder aus";
+            }
+
+            @Override
+            public Class<?> getParameterType() {
+                return Boolean.class;
+            }
+        });
+
+
+        // Strategie 2: Dimmen
+        this.functions.put("Helligkeit", new DeviceFunction() {
+            @Override
+            public void execute(Object parameter) {
+                if (parameter instanceof Integer) {
+                    helligkeit = (Integer) parameter;
+                    isAn = helligkeit > 0;
+                }
+            }
+
+            @Override
+            public String getDescription() {
+                return "Stellt die Helligkeit der Lampe ein";
+            }
+
+            @Override
+            public Class<?> getParameterType() {
+                return Integer.class;
+            }
+        });
+    }
+
+    @Override
+    public String getDeviceType() {
+        return "Lampe";
+    }
+
+    @Override
+    public String getCurrentState() {
+        return isAn ? "An (" + helligkeit + "%)" : "Aus";
+    }
+}
