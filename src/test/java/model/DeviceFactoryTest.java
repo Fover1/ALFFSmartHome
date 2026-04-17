3bfdde7b13c450b287f58687f2dbf12b2ed4f956
Fover <limberfinn@gmail.com>
2026-04-17 11:04:41 +0200

    adding tests

---
+++ src/test/java/model/DeviceFactoryTest.java
@@ -0,0 +1,47 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DeviceFactoryTest {
+
+    @Test
+    void testCreateDeviceSuccess() {
+        Room roomMock = Mockito.mock(Room.class);
+        String id = "1";
+        String name = "Wohnzimmerlampe";
+
+        AbstractDevice device = DeviceFactory.createDevice("Light", id, name, roomMock);
+
+        assertNotNull(device);
+        assertEquals(id, device.getId());
+        assertEquals(name, device.getName());
+        assertEquals(roomMock, device.getRoom());
+        assertEquals("devices.Light", device.getClass().getName());
+    }
+
+    @Test
+    void testCreateDeviceNotFound() {
+        Room mockRoom = Mockito.mock(Room.class);
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {
+            DeviceFactory.createDevice("NonExistentDevice", "2", "Name", mockRoom);
+        });
+
+        assertTrue(ex.getMessage().contains("Unnkown Devicetype"));
+    }
+
+    @Test
+    void testCreateDeviceError() {
+        Room mockRoom = Mockito.mock(Room.class);
+
+        assertThrows(RuntimeException.class, () -> {
+            DeviceFactory.createDevice("deviceScannerTest", "3", "Name", mockRoom);
+        });
+    }
+}
